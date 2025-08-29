9ad0ca5b77fa7af4902bb5892f342c25e69bb304
ychasser <41200686+ychasser@users.noreply.github.com>
2025-08-29 23:19:50 +0200

    Initial commit

---
+++ Main.java
@@ -0,0 +1,37 @@
+class Main {
+  public static void main(String[] args) {
+
+  // ----- Compter de 1 à 10
+    System.out.println("Compter de 1 à 10 :");
+
+
+  // ----- Compter de 0 à 100 par dizaine
+    System.out.println("Compter de 0 à 100 par 10 :");
+
+
+  // ---- Dans l’intervalle [1,50], nb divisibles par 3 et/ou 5
+    System.out.println("Nombre divisible par 3 et/ou 5");
+
+
+  // ---- table de multiplication
+    System.out.println("table de multiplication");
+
+
+  // ---- le nb de chiffre 2 entre 1 et 99
+    System.out.println("nb de chiffre 2");
+
+
+  // ---- qui suis-je (1) ?
+    System.out.println("Qui suis-je (1) ?");
+
+  // ---- qui suis-je (2) ?
+    System.out.println("Qui suis-je (2) ?");
+
+  // ---- le code du coffre fort
+    System.out.println("Code du coffre fort");
+
+  // ---- nombre 6 chiffres
+    System.out.println("Six chiffres");
+
+  }
+}
